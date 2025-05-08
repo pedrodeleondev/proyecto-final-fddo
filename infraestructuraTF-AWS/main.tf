f87b07f2c0e86856b13ade321bd7fb498f6f8ef6
FridaGarzaG <al07002961@tecmilenio.mx>
2025-05-08 15:58:09 -0600

    Creación de las subredes privadas y publicas, junto a tabla de rutas, asociación de tablas y grupo de subred RDS

--- infraestructuraTF-AWS/main.tf
+++ infraestructuraTF-AWS/main.tf
@@ -35,3 +35,83 @@ resource "aws_nat_gateway" "nat" {
     Name = "NAT Gateway - Proyecto"
   }
 }
+
+#Subred Publica Web
+resource "aws_subnet" "subred_publica_virginia_Web" {
+  vpc_id                  = aws_vpc.vpc_virginia.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Subred Publica Web - Proyecto"
+  }
+}
+
+#Subred Privada Backend
+resource "aws_subnet" "subred_privada_virginia_Back" {
+  vpc_id            = aws_vpc.vpc_virginia.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "Subred Privada Backend - Proyecto"
+  }
+}
+
+#Subred Privada BaseDatos
+resource "aws_subnet" "subred_privada_virginia_BD" {
+  vpc_id            = aws_vpc.vpc_virginia.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "Subred Privada BD - Proyecto"
+  }
+}
+
+#Grupo de subred RDS
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "db-subnet-group"
+  subnet_ids = [aws_subnet.subred_privada_virginia_Back.id, aws_subnet.subred_privada_virginia_BD.id]
+  tags = {
+    Name = "Grupo de subred BD - Proyecto"
+  }
+}
+
+#Tabla de rutas Pública
+resource "aws_route_table" "tabla_rutas_virginia" {
+  vpc_id = aws_vpc.vpc_virginia.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw-virginia.id
+  }
+  tags = {
+    Name = "Tabla Rutas Virginia"
+  }
+}
+
+#Tabla de rutas Privadas
+resource "aws_route_table" "tabla_rutas_privadas" {
+  vpc_id = aws_vpc.vpc_virginia.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = {
+    Name = "Tabla Rutas Privadas con NAT"
+  }
+}
+
+#Asociaciones de tabla de rutas
+resource "aws_route_table_association" "publica_virginia_Web" {
+  subnet_id      = aws_subnet.subred_publica_virginia_Web.id
+  route_table_id = aws_route_table.tabla_rutas_virginia.id
+}
+
+resource "aws_route_table_association" "privada_virginia_Backend" {
+  subnet_id      = aws_subnet.subred_privada_virginia_Back.id
+  route_table_id = aws_route_table.tabla_rutas_privadas.id
+}
+
+resource "aws_route_table_association" "privada_virginia_BD" {
+  subnet_id      = aws_subnet.subred_privada_virginia_BD.id
+  route_table_id = aws_route_table.tabla_rutas_privadas.id
+}
