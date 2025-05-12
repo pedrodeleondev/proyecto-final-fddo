2cc01d98aacc760c5cc7de5096aae31548a982dc
Pedro <114964122+pedrodeleondev@users.noreply.github.com>
2025-05-11 20:13:35 -0600

    Update cloud9-setup.sh

--- cloud9-setup.sh
+++ cloud9-setup.sh
@@ -14,24 +14,29 @@ terraform -version
 
 cd "$BASE_DIR"
 
-echo "📦 Clonando el repositorio si no existe..."
-[ ! -d proyecto-final-fddo ] && git clone https://github.com/pedrodeleondev/proyecto-final-fddo.git
-
+echo "📦 Clonando el repositorio..."
+git clone https://github.com/pedrodeleondev/proyecto-final-fddo.git
 cd proyecto-final-fddo/infraestructuraTF-AWS
 
 echo "🚀 Ejecutando Terraform..."
 terraform init -input=false
-terraform apply -auto-approve
+terraform plan -input=false -out=tfplan
+terraform apply -auto-approve tfplan
+rm -rf .terraform tfplan
 
 echo "✅ Infraestructura creada con éxito."
 
-# Extraer y mostrar datos RDS por referencia
+# Extraer datos RDS
 DB_HOST=$(terraform output -raw rds_endpoint | cut -d':' -f1)
 DB_USER=$(terraform output -raw rds_username)
 DB_NAME=$(terraform output -raw rds_database_name)
+DB_PASSWORD="proyecto98765"  # Debe coincidir con lo definido en main.tf
 
 echo ""
 echo "📤 Datos de conexión a la base de datos:"
 echo "HOST: $DB_HOST"
 echo "USER: $DB_USER"
 echo "DB: $DB_NAME"
+
+echo ""
+echo "✅ Todo listo. La instancia EC2 se encargará del resto automáticamente."
