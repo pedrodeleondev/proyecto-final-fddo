e8c854574b4380626b36faeaac3cb77fb4389947
Pedro <114964122+pedrodeleondev@users.noreply.github.com>
2025-05-11 19:52:53 -0600

    Update cloud9-setup.sh

--- cloud9-setup.sh
+++ cloud9-setup.sh
@@ -22,16 +22,35 @@ echo "🚀 Ejecutando Terraform..."
 terraform init -input=false
 terraform plan -input=false -out=tfplan
 terraform apply -auto-approve tfplan
+rm -rf .terraform tfplan
 
 echo "✅ Infraestructura creada con éxito."
 
-# Extraer datos de la DB para que los uses si quieres editarlos a mano en EC2
+# Extraer datos RDS
 DB_HOST=$(terraform output -raw rds_endpoint | cut -d':' -f1)
 DB_USER=$(terraform output -raw rds_username)
 DB_NAME=$(terraform output -raw rds_database_name)
+DB_PASSWORD="proyecto98765"  # Debe coincidir con lo definido en main.tf
 
 echo ""
 echo "📤 Datos de conexión a la base de datos:"
 echo "HOST: $DB_HOST"
 echo "USER: $DB_USER"
 echo "DB: $DB_NAME"
+
+# Crear archivo .env
+echo "📝 Generando archivo db.env..."
+cat <<EOF > db.env
+DB_HOST=$DB_HOST
+DB_USER=$DB_USER
+DB_PASSWORD=$DB_PASSWORD
+DB_NAME=$DB_NAME
+EOF
+
+echo ""
+read -p "🌍 Ingresa la IP pública de tu instancia EC2 (ej. 44.201.12.34): " EC2_IP
+
+echo "📦 Enviando archivo db.env a la instancia EC2..."
+scp -i vockey.pem db.env ec2-user@$EC2_IP:/home/ec2-user/db.env
+
+echo "✅ Archivo enviado con éxito. Ahora corre ec2-setup.sh en tu instancia EC2."
